342e542f918a427aa5bf836820f64adf148ead63
kanhaiyagithub <127029995+kanhaiyagithub@users.noreply.github.com>
2024-12-16 23:31:30 +0530

    Add files via upload

---
+++ practiceSet1/solution.js
@@ -0,0 +1,11 @@
+console.log(`your final bill is${((1*100)+(2*10)+(1*30))}`);
+console.log((79990+2*24990)*18/100);
+//create a string using all three methods 
+console.log('hey i am kanhaiya kumar');
+console.log(`hey i am kanhaiya kumar`);
+console.log("hey i am kanhaiya kumar");
+//concinate a string and number 
+console.log("hey i am kanhaiya kumar"+5);
+console.log(`your final myntra bag amount is₹${((1*100)+(2*10)+(1*30))}`);
+alert(`your final myntra bag amount is₹${((1*100)+(2*10)+(1*30))}`);
+
